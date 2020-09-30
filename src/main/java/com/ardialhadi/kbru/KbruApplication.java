2470d2d6219aa83a6e3988adedcc424139b55b0b
Ardi AL Hadi <alhadi.ardi@gmail.com>
2020-09-30 07:37:19 +0700

    first commit

---
+++ src/main/java/com/ardialhadi/kbru/KbruApplication.java
@@ -0,0 +1,13 @@
+package com.ardialhadi.kbru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ardialhadi/kbru/KbruApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ardialhadi.kbru;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KbruApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
